e8c7af104aca9000c672d82fb21e1ca099011b42
saltana67 <saltana67@gmail.com>
2025-01-27 17:01:43 +0100

    added tabs for text and json view

--- src/main/java/com/example/application/views/jsonviewer/JsonViewerView.java
+++ src/main/java/com/example/application/views/jsonviewer/JsonViewerView.java
@@ -2,6 +2,8 @@ package com.example.application.views.jsonviewer;
 
 import com.ironapi.vaadin.jsonviewer.JsonViewer;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.TabSheet;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -14,12 +16,33 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 @AnonymousAllowed
 public class JsonViewerView extends VerticalLayout {
 
+	TabSheet tabSheet = null;
+	TextArea ta = null;
+	JsonViewer jsonViewer = null;
+	
 	public JsonViewerView() {
 		setMargin(true);
 		
-		JsonViewer jsonViewer = new JsonViewer();
-		add(jsonViewer);
+		ta = new TextArea();
+		ta.setSizeFull();
+		
+		
+		jsonViewer = new JsonViewer();
+
+		tabSheet = new TabSheet();
+		tabSheet.setSizeFull();
+		
+		tabSheet.add("Text",ta);
+		tabSheet.add("Json",jsonViewer);
+
+		add(tabSheet);
+		
 		setHorizontalComponentAlignment(Alignment.CENTER,jsonViewer);
 		
+		ta.addValueChangeListener(event -> {
+		    System.out.println("Value changed to: " + event.getValue());
+		    jsonViewer.setJsonString(event.getValue());
+		});
 	}
 }
+
