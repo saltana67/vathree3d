76c906e3684231ae99615fe86f9c1879de1c2866
saltana67 <saltana67@gmail.com>
2025-01-27 17:01:08 +0100

    added json  string value setter

--- src/main/java/com/ironapi/vaadin/jsonviewer/JsonViewer.java
+++ src/main/java/com/ironapi/vaadin/jsonviewer/JsonViewer.java
@@ -1,5 +1,7 @@
 package com.ironapi.vaadin.jsonviewer;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -10,8 +12,23 @@ import com.vaadin.flow.component.dependency.JsModule;
 @Tag("pre")
 public class JsonViewer extends Component {
 
+	private static AtomicInteger nextIndex = new AtomicInteger(1);
+	
+	private static String getNextID() {
+		return "json-viewer-" + nextIndex.getAndIncrement();
+	}
+
+	String elementId = null;
+	String jsonString = "";
+	
 	public JsonViewer() {
+		//elementId = getNextID();
+		//setId(elementId);
 		getElement().executeJs("window.initJsonViewer($0)", this);
 	}
 
+	public void setJsonString(String jsonString) {
+		this.jsonString = jsonString;
+		getElement().executeJs("window.JsonViewerSetString($0,$1)", this, this.jsonString);
+	}
 }
